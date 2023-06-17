85b13401e72d45b0ca7a158585a4fa8f4f296be0
alex2002572 <136887066+alex2002572@users.noreply.github.com>
2023-06-17 16:33:48 +0500

    Add files via upload

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,31 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+  let arr;
+
+  arr = [];
+  let discriminant = Math.pow(b, 2) - 4 * a * c;
+  if (discriminant < 0) {
+    arr;
+  } else if (discriminant === 0) {
+    arr.push(-b / (2 * a));
+  } else {
+    arr.push((-b + Math.sqrt(discriminant)) / (2 * a))
+    arr.push((-b - Math.sqrt(discriminant)) / (2 * a))
+    console.log(arr)
+  }
+  return arr; // array
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+function calculateTotalMortgage(percent, contribution, amount, countMonths) 
+{
+	const P = percent / 100 / 12;
+	const S = amount - contribution;
+	const n = countMonths;
+	const monthlyPayment = S * (P + P / (Math.pow(1 + P, n) - 1));
+	const totalAmount = (monthlyPayment * n).toFixed(2);
+	let num = Number(totalAmount);
+	return num;
 }
+calculateTotalMortgage(10, 0, 50000, 12)
