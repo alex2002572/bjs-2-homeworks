d1deb161f3500afc867b4940af8d2a3791ccabf4
alex2002572 <136887066+alex2002572@users.noreply.github.com>
2023-06-26 14:49:49 +0500

    Add files via upload

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,7 @@
-function compareArrays(arr1, arr2) {
-  
-}
+const compareArrays = (arr1, arr2) => arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
+
 
 function getUsersNamesInAgeRange(users, gender) {
-  
-}
+    let result = users.filter(el => el.gender === gender).reduce((accum, averageAge, index, arrayLength) => accum + averageAge.age / arrayLength.length, 0);
+    return result 
+};
